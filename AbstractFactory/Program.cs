0aa35d001998925afd16626c5572a820317c87f1
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-07-04 15:54:59 -0300

    Usando instruções de nível superior (top level programs)

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,31 +1,22 @@
 ﻿using AbstractFactory.Domain.Enums;
-using AbstractFactory.Domain.Products;
 using AbstractFactory.Factory;
 using System;
 
-namespace AbstractFactory
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Obtém as fábricas
-            MassasAbstractFactory boloFactory = MassasAbstractFactory.CriarFabricaDeMassa(TipoMassa.Bolo);
-            MassasAbstractFactory pizzaFactory = MassasAbstractFactory.CriarFabricaDeMassa(TipoMassa.Pizza);
 
-            // Usando factory de bolo
-            var boloChocolate = boloFactory.CriarMassa((TipoMassa)TipoBolo.Chocolate);
-            var boloLaranja = boloFactory.CriarMassa((TipoMassa)TipoBolo.Laranja);
+// Obtém as fábricas
+MassasAbstractFactory boloFactory = MassasAbstractFactory.CriarFabricaDeMassa(TipoMassa.Bolo);
+MassasAbstractFactory pizzaFactory = MassasAbstractFactory.CriarFabricaDeMassa(TipoMassa.Pizza);
 
-            // Usando factory de pizza
-            var pizzaMussarela = pizzaFactory.CriarMassa((TipoMassa)TipoPizza.Mussarela);
-            var pizzaCalabresa = pizzaFactory.CriarMassa((TipoMassa)TipoPizza.Calabresa);
+// Usando factory de bolo
+var boloChocolate = boloFactory.CriarMassa((TipoMassa)TipoBolo.Chocolate);
+var boloLaranja = boloFactory.CriarMassa((TipoMassa)TipoBolo.Laranja);
 
-            // Exibe os detalhes
-            Console.WriteLine(boloChocolate.ToString());
-            Console.WriteLine(boloLaranja.ToString());
-            Console.WriteLine(pizzaMussarela.ToString());
-            Console.WriteLine(pizzaCalabresa.ToString());
-        }
-    }
-}
+// Usando factory de pizza
+var pizzaMussarela = pizzaFactory.CriarMassa((TipoMassa)TipoPizza.Mussarela);
+var pizzaCalabresa = pizzaFactory.CriarMassa((TipoMassa)TipoPizza.Calabresa);
+
+// Exibe os detalhes
+Console.WriteLine(boloChocolate.ToString());
+Console.WriteLine(boloLaranja.ToString());
+Console.WriteLine(pizzaMussarela.ToString());
+Console.WriteLine(pizzaCalabresa.ToString());
